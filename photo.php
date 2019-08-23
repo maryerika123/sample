cb241ccc0c0418ce86cdcbeb40a575946ca02cab
maryerika123 <53499740+maryerika123@users.noreply.github.com>
2019-08-23 23:55:10 +0800

    Create photo.php

---
+++ photo.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+<center>
+<h1>AtregenioM photo Gallery<h1>
+<h2>Photo 1</h2>
+    <style>
+div.fixed {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+}
+.bottomleft {
+  position: absolute;
+  bottom: 8px;
+  left: 16px;
+  font-size: 18px;
+}
+</style>
+    </body>
+<img src="1.png" alt="erika" style="width:400px;height:400px;">
+</center>
+
+        <div class=".bottomleft">
+        <a href="home.php">Back to home</a> 
+    
+       <div class="fixed">
+        <a href="photo2.php">Next Photo</a> 
+</div> 
+       
+</html>
