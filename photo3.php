547caf5c1f4089a296e1e0e62367e9c76ca7bbd6
maryerika123 <53499740+maryerika123@users.noreply.github.com>
2019-08-23 23:58:31 +0800

    Create photo3.php

---
+++ photo3.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+	
+
+	<html>
+	
+
+	<body>
+	<center>
+	<h1>AtregenioM photo Gallery<h1>
+	<h2>photo 3  </h2>
+	 <style>
+	
+	
+	.bottomleft {
+	  position: absolute;
+	  bottom: 8px;
+	  left: 16px;
+	  font-size: 18px;
+	
+	
+	}
+	</style>   
+	    
+	</body>
+	<img src="3.jpg" alt="erika" style="width:400px;height:400px;">
+	
+
+	
+
+	        <div class="bottomleft">
+	        <a href="home.php">Back to home</a> 
+	    
+	      
+	</body>      
+	</html>
+
